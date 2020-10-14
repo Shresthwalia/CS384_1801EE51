2d5fdb64319d23da6046c3f70220b97301163d40
Shrestha <sauravwaliajs@gmail.com>
2020-10-14 17:00:21 +0530

    Coded the gender function according to male and female

---
+++ Assignment3/tutorial03.py
@@ -0,0 +1,69 @@
+import csv
+import os
+os.system('cls')
+
+def course():
+    # Read csv and process
+    pass
+
+
+def country():
+    # Read csv and process
+    pass
+
+
+def email_domain_extract():
+    # Read csv and process
+    pass
+
+
+def gender():
+    # Read csv and process
+    mainlist = open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/studentinfo_cs384.csv', 'r')
+    male = open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/male.csv','w').close()
+    female =open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/female.csv','w').close()
+    with mainlist:
+        reader=csv.reader(mainlist)
+        for row in reader:
+            if(row[4]=='Male'):
+                male = open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/male.csv','a',newline='')
+                with male:
+                    mw=csv.writer(male)
+                    mw.writerow(row)
+            elif(row[4]=='Female'):
+                female =open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/female.csv','a',newline='')
+                with female:
+                    fw=csv.writer(female)
+                    fw.writerow(row)
+            else:
+                male = open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/male.csv','a',newline='')
+                with male:
+                    mw=csv.writer(male)
+                    mw.writerow(row) 
+                female =open('C:/Users/Shrestha Walia/CS384_1801EE51/Assignment3/analytics/gender/female.csv','a',newline='')
+                with female:
+                    fw=csv.writer(female)
+                    fw.writerow(row)
+                          
+    pass
+
+gender()
+def dob():
+    # Read csv and process
+    pass
+
+
+def state():
+    # Read csv and process
+    pass
+
+
+def blood_group():
+    # Read csv and process
+    pass
+
+
+# Create the new file here and also sort it in this function only.
+def new_file_sort():
+    # Read csv and process
+    pass
