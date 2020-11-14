2c715df57731ae73935b93e64b08225fe2d3c2ee
Shrestha <sauravwaliajs@gmail.com>
2020-11-14 23:00:06 +0530

    wrote program for renaming and sorting the tv series videos and subtitles in a specific way

---
+++ Assignment5/tutorial05.py
@@ -0,0 +1,167 @@
+import os
+import re
+
+
+def rename_FIR(paddingepisode,paddingseason=0):
+    # rename Logic
+    path = r'./Subtitles/FIR'
+    episodeno = ''
+    for filename in os.listdir(path):
+        pattern = re.compile(r'\d+')
+        list = re.findall(pattern, filename)
+        episodeno = list[0]
+        while(len(episodeno) < paddingepisode):
+            episodeno = '0'+episodeno
+        if(len(episodeno) > paddingepisode):
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            if not os.path.isfile(path+'/'+'FIR'+' - Episode '+episodeno+'.mp4'):
+                os.rename(path+'/'+filename, path+'/'+'FIR' +
+                          ' - Episode '+episodeno+'.mp4')
+            else:
+                os.remove(path+'/'+filename)
+        if(filename.endswith('.srt')):
+            if not os.path.isfile(path+'/'+'FIR'+' - Episode '+episodeno+'.srt'):
+                os.rename(path+'/'+filename, path+'/'+'FIR' +
+                          ' - Episode '+episodeno+'.srt')
+            else:
+                os.remove(path+'/'+filename)
+    pass
+
+
+def rename_Game_of_Thrones(paddingseason, paddingepisode):
+    # rename Logic
+    path = r'./Subtitles/Game of Thrones'
+    seasonno = ''
+    episodeno = ''
+    episodename = ''
+    for filename in os.listdir(path):
+        lis = filename.split(' - ')
+        seasonno = lis[1][0:lis[1].index('x')]
+        episodeno = lis[1][lis[1].index('x')+1:]
+        episodename = lis[2][0:lis[2].index('.')]
+        while(len(seasonno) < paddingseason):
+            seasonno = '0'+seasonno
+        if(len(seasonno) > paddingseason):
+            seasonno = seasonno[-1*paddingseason:]
+        while(len(episodeno) < paddingepisode):
+            episodeno = '0'+episodeno
+        if(len(episodeno) > paddingepisode):
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            os.rename(path+'/'+filename, path+'/' +
+                      lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4')
+        if(filename.endswith('.srt')):
+            os.rename(path+'/'+filename, path+'/' +
+                      lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt')
+    pass
+
+
+def rename_Sherlock(paddingseason, paddingepisode):
+    # rename Logic
+    path = r'./Subtitles/Sherlock'
+    seasonno = ''
+    episodeno = ''
+    for filename in os.listdir(path):
+        temp = filename[filename.index('.')+1:]
+        seasonno = temp[1]+temp[2]
+        temp2 = temp[temp.index('E')+1:]
+        episodeno = temp2[0]+temp2[1]
+        while(len(seasonno) < paddingseason):
+            seasonno = '0'+seasonno
+        if(len(seasonno) > paddingseason):
+            seasonno = seasonno[-1*paddingseason:]
+        while(len(episodeno) < paddingepisode):
+            episodeno = '0'+episodeno
+        if(len(episodeno) > paddingepisode):
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            os.rename(path+'/'+filename, path+'/'+'Sherlock' +
+                      ' - Season '+seasonno+' Episode '+episodeno+'.mp4')
+        if(filename.endswith('.srt')):
+            os.rename(path+'/'+filename, path+'/'+'Sherlock' +
+                      ' - Season '+seasonno+' Episode '+episodeno+'.srt')
+    pass
+
+
+def rename_Suits(paddingseason, paddingepisode):
+    # rename Logic
+    path = r'./Subtitles/Suits'
+    seasonno = ''
+    episodeno = ''
+    episodename = ''
+    for filename in os.listdir(path):
+        lis = filename.split(' - ')
+        seasonno = lis[1][0:lis[1].index('x')]
+        episodeno = lis[1][lis[1].index('x')+1:]
+        temp1 = lis[2].split('.480')
+        temp2 = temp1[0].split('.720')
+        temp3 = temp2[0].split('.1080')
+        temp4 = temp3[0].split('.HDTV')
+        episodename = temp4[0].split('.en')[0]
+        while(len(seasonno) < paddingseason):
+            seasonno = '0'+seasonno
+        if(len(seasonno) > paddingseason):
+            seasonno = seasonno[-1*paddingseason:]
+        while(len(episodeno) < paddingepisode):
+            episodeno = '0'+episodeno
+        if(len(episodeno) > paddingepisode):
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4')
+            else:
+                os.remove(path+'/'+filename)
+        if(filename.endswith('.srt')):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt')
+            else:
+                os.remove(path+'/'+filename)
+    pass
+
+
+def rename_How_I_Met_Your_Mother(paddingseason, paddingepisode):
+    # rename Logic
+    path = r'./Subtitles/How I Met Your Mother'
+    seasonno = ''
+    episodeno = ''
+    episodename = ''
+    for filename in os.listdir(path):
+        lis = filename.split(' - ')
+        seasonno = lis[1][0:lis[1].index('x')]
+        episodeno = lis[1][lis[1].index('x')+1:]
+        temp1 = lis[2].split('.480')
+        temp2 = temp1[0].split('.720')
+        temp3 = temp2[0].split('.1080')
+        temp4 = temp3[0].split('.HDTV')
+        episodename = temp4[0].split('.en')[0]
+        while(len(seasonno) < paddingseason):
+            seasonno = '0'+seasonno
+        if(len(seasonno) > paddingseason):
+            seasonno = seasonno[-1*paddingseason:]
+        while(len(episodeno) < paddingepisode):
+            episodeno = '0'+episodeno
+        if(len(episodeno) > paddingepisode):
+            episodeno = episodeno[-1*paddingepisode:]
+        if filename.endswith(".mp4"):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.mp4')
+            else:
+                os.remove(path+'/'+filename)
+        if(filename.endswith('.srt')):
+            if not os.path.isfile(path+'/'+lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt'):
+                os.rename(path+'/'+filename, path+'/' +
+                          lis[0]+' - Season '+seasonno+' Episode '+episodeno+' - '+episodename+'.srt')
+            else:
+                os.remove(path+'/'+filename)
+    pass
+
+
+rename_FIR(6)
+rename_Game_of_Thrones(3, 3)
+rename_How_I_Met_Your_Mother(3, 3)
+rename_Sherlock(3, 3)
+rename_Suits(3, 3)
